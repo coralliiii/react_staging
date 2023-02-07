6cf66972ce5983dcd34005da3638142aaf912c31
CoralLi <944284279@qq.com>
2023-02-08 00:29:29 +0800

    20230207

--- 08_src_路由的基本使用/App.jsx
+++ 08_src_路由的基本使用/App.jsx
@@ -1,9 +1,7 @@
 import React, { Component } from 'react'
-import { Route } from "react-router-dom"
-import Home from './pages/home'//路由组件
-import About from './pages/about'
-import Header from './components/Header'
-import MyNavLink from './components/MyNavLink'
+import { Link, Route } from "react-router-dom"
+import Home from './components/home'
+import About from './components/about'
 
 export default class App extends Component {
 
@@ -12,7 +10,7 @@ export default class App extends Component {
 			<div>
 				<div className="row">
 					<div className="col-xs-offset-2 col-xs-8">
-						<Header />
+						<div className="page-header"><h2>React Router Demo</h2></div>
 					</div>
 				</div>
 				<div className="row">
@@ -23,10 +21,8 @@ export default class App extends Component {
 							<a className="list-group-item active" href="./home.html">Home</a> */}
 
 							{/* 在React中靠路由链接实现切换组件 */}
-							{/* <NavLink activeClassName='hogwards' className='list-group-item' to="/about">About</NavLink>
-							<NavLink activeClassName='hogwards' className='list-group-item' to="/home">Home</NavLink> */}
-							<MyNavLink to="/about">About</MyNavLink>
-							<MyNavLink to="/home">Home</MyNavLink>
+							<Link className='list-group-item' to="/about">About</Link>
+							<Link className='list-group-item' to="/home">Home</Link>
 						</div>
 					</div>
 					<div className="col-xs-6">
--- 09_src_NavLink的使用/App.jsx
+++ 09_src_NavLink的使用/App.jsx
@@ -1,9 +1,8 @@
 import React, { Component } from 'react'
-import { Route } from "react-router-dom"
+import { NavLink, Route } from "react-router-dom"
 import Home from './pages/home'//路由组件
 import About from './pages/about'
 import Header from './components/Header'
-import MyNavLink from './components/MyNavLink'
 
 export default class App extends Component {
 
@@ -23,10 +22,8 @@ export default class App extends Component {
 							<a className="list-group-item active" href="./home.html">Home</a> */}
 
 							{/* 在React中靠路由链接实现切换组件 */}
-							{/* <NavLink activeClassName='hogwards' className='list-group-item' to="/about">About</NavLink>
-							<NavLink activeClassName='hogwards' className='list-group-item' to="/home">Home</NavLink> */}
-							<MyNavLink to="/about">About</MyNavLink>
-							<MyNavLink to="/home">Home</MyNavLink>
+							<NavLink activeClassName='hogwards' className='list-group-item' to="/about">About</NavLink>
+							<NavLink activeClassName='hogwards' className='list-group-item' to="/home">Home</NavLink>
 						</div>
 					</div>
 					<div className="col-xs-6">
